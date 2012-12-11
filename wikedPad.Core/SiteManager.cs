9197d1a2fc8a3993bebb0a3d4aceec3809f73ddc
Thomas PIERRAIN <thomas@pierrain.net>
2012-12-11 01:59:47 +0100

    - Add the wikedPad.Tests and wikedPad.Core projects
    - Configure a common Settings.StyleCop for the whole project, another for the unt test project, and fix all stylecop warning
    - Include a wiked! sample for unit test purpose

---
+++ wikedPad.Core/SiteManager.cs
@@ -0,0 +1,25 @@
+﻿namespace WikedPad.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Handles basic information about the wiked! site
+    /// </summary>
+    public class SiteManager
+    {
+        private string url;
+
+        public SiteManager(string url)
+        {
+            this.url = url;
+            if (!File.Exists(url))
+            {
+                throw new InvalidOperationException("Could not find the pom.xml file for this wiked! site.");
+            }
+        }
+    }
+}
--- wikedPad/Properties/Resources.Designer.cs
+++ wikedPad/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace wikedPad.Properties {
+namespace WikedPad.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace wikedPad.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("wikedPad.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WikedPad.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
